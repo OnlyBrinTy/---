299eb7745158c8e9a269849f9b71f08a77d0e5a3
Рома <onlybrinty@gmail.com>
2023-02-17 17:55:06 +0300

    Двойная защита

--- 5/astronauts_selection.py
+++ 5/astronauts_selection.py
@@ -5,9 +5,20 @@ app = Flask(__name__)
 
 @app.route('/astronauts_selection', methods=['POST', 'GET'])
 def do_thing():
-    param = {}
-    param['titlle'] = 'рварвар'
-    return render_template('selection.html', **param)
+    if request.method == 'GET':
+        param = {}
+        param['titlle'] = 'Форма'
+        return render_template('selection.html', **param)
+    elif request.method == 'POST':
+        print(request.form['email'])
+        print(request.form['password'])
+        print(request.form['class'])
+        print(request.form['file'])
+        print(request.form['about'])
+        print(request.form['accept'])
+        print(request.form['sex'])
+        return "Форма отправлена"
+
 
 
 if __name__ == '__main__':
